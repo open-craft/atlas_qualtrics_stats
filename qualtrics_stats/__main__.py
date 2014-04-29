51bba24bd3b4b6f088dfe1e96a0428a620be9c9b
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-04-29 03:59:28 +0200

    Reintroduce locking in cron execution

--- qualtrics_stats/__main__.py
+++ qualtrics_stats/__main__.py
@@ -17,30 +17,28 @@ General options:
 """
 
 import logging
+import sys
 from docopt import docopt
-# from lockfile import FileLock
-
-from .generate import QualtricsStats
-from .cron import cron
+from lockfile import FileLock
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s [%(levelname)s] %(message)s')
 
 
-arguments = docopt(__doc__, version='Qualtrics Stats 0.3')
+arguments = docopt(__doc__, version='Qualtrics Stats 0.3.1')
 
 if arguments['generate']:
+    from .generate import QualtricsStats
     csv_override = open(arguments['--override']) if arguments['--override'] else None
     QS = QualtricsStats(arguments['<survey_xml_spec>'], csv_override)
     print(QS.run())
 
 if arguments['cron']:
+    from .cron import cron
     csv_override = open(arguments['--override']) if arguments['--override'] else None
-    cron(csv_override)
-
-# lock = FileLock("/tmp/qualtrics_stats.lock")
-# if lock.is_locked():
-#     logging.error('Lockfile locked, exiting.')
-#     sys.exit(1)
-# with lock:
-#     main()
+    lock = FileLock("/tmp/qualtrics_stats.lock")
+    if lock.is_locked():
+        logging.error('Lockfile locked, exiting.')
+        sys.exit(1)
+    with lock:
+        cron(csv_override)
